1280e79e70e802c7314bdbe3bc172106568cc496
youhandan <youhandan@163.com>
2017-12-18 20:24:32 +0800

    Refactor TaskStatus component.

--- src/components/AgentsContainer/TasksStatus.js
+++ src/components/AgentsContainer/TasksStatus.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Header, List } from 'semantic-ui-react'
-import { partition, size, map } from 'lodash'
+import { Header, List, Grid } from 'semantic-ui-react'
+import { partition, size, map, groupBy } from 'lodash'
 
 const items = new Array(10).fill('bjstdmngbgr02/Acceptance_test')
 
@@ -15,17 +15,28 @@ export default class TasksStatus extends React.Component {
   }
 
   render() {
-    const listItem = map(items, (item, key) => <List.Item key={key}>{item}</List.Item>)
-    const groupedByStatusTasks = partition(this.props.tasksStatus, {status: 'building'})
-    const buildingTasks = size(groupedByStatusTasks[0])
-    const idleTasks = size(groupedByStatusTasks[1])
+    const taskHistoryListItems = map(items, (item, key) => <List.Item key={key}>{item}</List.Item>)
+    const tasksStatusCategories = groupBy(this.props.tasksStatus, 'status')
+    const taskStatusListItems = map(tasksStatusCategories, (tasks, key) => {
+      return (
+        <List.Item key={key}>
+          <Grid columns={2}>
+            <Grid.Column>
+              {key}
+            </Grid.Column>
+            <Grid.Column>
+              {size(tasks)}
+            </Grid.Column>
+          </Grid>
+        </List.Item>
+      )
+    })
     return (
       <div>
         <Header as='h3' dividing >Summary</Header>
-        <p>building {buildingTasks}</p>
-        <p>idle {idleTasks}</p>
+        <List>{taskStatusListItems}</List>
         <Header as='h3' dividing>History</Header>
-        <List>{listItem}</List>
+        <List>{taskHistoryListItems}</List>
       </div>
     )
   }
